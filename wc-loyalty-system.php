0eafc4c9e54f1ba593456160270bb012a75f65d9
Md Mahbub Islam <atthemahbub@gmail.com>
2025-12-31 12:14:40 +0600

    Create plugin structure and metadata.

---
+++ wc-loyalty-system.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Loyalty System
+ * Plugin URI: #
+ * Description: Simple loyalty points system for WooCommerce.
+ * Version: 1.0.0
+ * Author: Md Mahbub Islam
+ * Text Domain: wc-loyalty-system
+ * **/
